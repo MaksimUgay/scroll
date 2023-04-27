78fd0d6b1c274bb370e0060f23aad64f6760998e
Maksim Ugay <maxim9733012m@gmail.com>
2023-04-27 22:21:45 +0500

    v1.1

--- main.js
+++ main.js
@@ -41,35 +41,27 @@ let myScroll = new Scroll({
     unit: '%'
 });
 
-let header = document.querySelector('.header');
-let headerTitle = document.querySelector('.header__content');
+class Header {
+    constructor (header,headerTitle) {
+        this.item = document.querySelector(headerTitle);
+        this.field = document.querySelector(header);
+        this.item.addEventListener('mouseover', () => this.moveHeader());
+    }
 
-headerTitle.addEventListener('mouseover',() => {
-    moveHeader();
-});
+    moveHeader() {
+        this.width = window.innerWidth - this.item.offsetWidth;
+        this.height = window.innerHeight - this.item.offsetHeight;
+        this.x = rand(0, this.width);
+        this.y = rand(0, this.height);
 
-function moveHeader() {
-    headerTitle.style.width = 233 + 'px';
-    headerTitle.style.height = 55 + 'px';
-    let sizeX = headerTitle.style.width.split('p')[0];
-    let sizeY = headerTitle.style.height.split('p')[0];
-
-
-    let cor = header.getBoundingClientRect();
-    let {width, height} = cor;
-
-
-    let x = rand(0, width - sizeX);
-    let y = rand(0, height - sizeY);
-
-    headerTitle.style.top = y + 'px';
-    headerTitle.style.left = x + 'px';
-
-    console.log(sizeX);
+        this.item.style.position = 'absolute';
+        this.item.style.top = this.y + 'px';
+        this.item.style.left = this.x + 'px';
+    }
 }
+
+let myHeader = new Header('.header','.header__content');
 
 function rand(min,max) {
     return Math.floor(Math.random() * ( max + 1 - min ) + min);
 }
-// let cor = header.getBoundingClientRect();
-// console.log(cor);
